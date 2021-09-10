5cbc1aff355c4d4d1f9e507d440897fc5c152800
sanctum006 <74344331+sanctum006@users.noreply.github.com>
2021-09-10 23:17:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,18 +37,84 @@
 //  (document.getElementById("answer").value)= res;
 
 var principle = document.getElementById("cal-principle");
-var rate = document.getElementById("cal-rate");      
-var time = document.getElementById("cal-time");      
+var rate = document.getElementById("cal-rate");
+var time = document.getElementById("cal-time");
 
 function compound_intrest() {
-   var first_number = parseFloat(principle.value);
-   if (isNaN(first_number)) first_number = 0;
-   var second_number = parseFloat(rate.value);
-   if (isNaN(second_number)) second_number = 0;
-   var third_number = parseFloat(time.value);
-   if (isNaN(third_number)) third_number = 0;
-//    var result = (first_number *(Math.pow((1+(second_number/100)),(third_number))))-first_number;
-   var result = (first_number *(Math.pow((1+(second_number/100)),(third_number))));
-   var result=Math.round(result);
-   document.getElementById("txtresult").value = result;
+  var first_number = parseFloat(principle.value);
+  if (isNaN(first_number)) first_number = 0;
+  var second_number = parseFloat(rate.value);
+  if (isNaN(second_number)) second_number = 0;
+  var third_number = parseFloat(time.value);
+  if (isNaN(third_number)) third_number = 0;
+  //    var result = (first_number *(Math.pow((1+(second_number/100)),(third_number))))-first_number;
+  var result = first_number * Math.pow(1 + second_number / 100, third_number);
+  var result = Math.round(result);
+  document.getElementById("txtresult").value = result;
 }
+
+// Calculator
+
+let initialSlider = document.getElementById("initialSlider");
+let autoSlider = document.getElementById("autoSlider");
+let lengthSlider = document.getElementById("lengthSlider");
+
+let initialValue = document.getElementById("initialValue");
+let autoValue = document.getElementById("autoValue");
+let lengthValue = document.getElementById("lengthValue");
+
+let totalValue = document.getElementById("totalValue");
+
+let interest = 12;
+let timeInterval = 12;
+
+// Compound Interest Calculator
+const compoundInterest = (p, t, r, n) => {
+  const amount = p * Math.pow(1 + r / n, n * t);
+  return amount;
+};
+
+const sip = (p, r, n) => {
+  let i = r / (100 * n);
+  const amount = p * (1 + i) * ((Math.pow(1 + i, n) - 1) / i);
+  return amount;
+};
+
+initialValue.textContent = "₹" + initialSlider.value;
+autoValue.textContent = "₹" + autoSlider.value;
+totalValue.textContent =
+  "₹ " +
+  compoundInterest(initialSlider.value, lengthSlider.value, 0.052, 1).toFixed(
+    2
+  );
+
+lengthValue.textContent = `${lengthSlider.value} ${
+  lengthSlider.value > 1 ? "years" : "year"
+}`;
+
+initialSlider.oninput = function () {
+  initialValue.textContent = "₹" + initialSlider.value;
+  totalValue.textContent = `₹ ${(
+    compoundInterest(initialSlider.value, lengthSlider.value, 0.052, 1) +
+    sip(autoSlider.value, interest, timeInterval)
+  ).toFixed(2)}`;
+};
+
+autoSlider.oninput = function () {
+  autoValue.textContent = "₹" + autoSlider.value;
+  totalValue.textContent = `₹ ${(
+    compoundInterest(initialSlider.value, lengthSlider.value, 0.052, 1) +
+    sip(autoSlider.value, interest, timeInterval)
+  ).toFixed(2)}`;
+};
+
+lengthSlider.oninput = function () {
+  lengthValue.textContent = `${lengthSlider.value} ${
+    lengthSlider.value > 1 ? "years" : "year"
+  }`;
+
+  totalValue.textContent = `₹ ${(
+    compoundInterest(initialSlider.value, lengthSlider.value, 0.052, 1) +
+    sip(autoSlider.value, interest, timeInterval)
+  ).toFixed(2)}`;
+};
